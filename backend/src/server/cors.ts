0abf9eb48328ae06fc68effe148ac84deaf3b52d
Ana Coronel <a9nna.coronel@gmail.com>
2023-07-15 18:25:08 +0200

    Install cors package and set allowed origins

---
+++ backend/src/server/cors.ts
@@ -0,0 +1,12 @@
+import "../loadEnvironments.js";
+import type cors from "cors";
+
+const allowedOrigins = [
+  "http://localhost:3000",
+];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+export default options;
