db1b94cdc67a802da0459ea937cb37d6723bd48e
Ana Coronel <a9nna.coronel@gmail.com>
2023-07-27 21:12:20 +0200

    Create connectDatabase function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,19 @@ import "./loadEnvironments.js";
 import createDebug from "debug";
 import chalk from "chalk";
 import startServer from "./server/startServer.js";
+import connectDatabase from "./database/connectDatabase.js";
 
 const debug = createDebug("app");
 const port = process.env.PORT!;
+const url = process.env.DATABASE_CONNECTION_URL!;
 
 try {
   await startServer(Number(port));
   debug(chalk.bgGreen(`Start with server 'http://localhost:${port}'`));
+
+  await connectDatabase(url);
+  debug(chalk.bgGreen("Connected to database"));
+  
 } catch (error) {
   debug(chalk.bgRed(error.message));
 }
