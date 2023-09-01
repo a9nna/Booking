d880511c0c1ec6b034cf34e5ce760e676970b365
Ana Coronel <a9nna.coronel@gmail.com>
2023-09-01 21:40:06 +0200

    Create a createTrip controller

--- backend/src/server/controllers/tripControllers.ts
+++ backend/src/server/controllers/tripControllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import Trip from "../../database/models/Trip.js";
 import { CustomError } from "../../CustomError/CustomError.js";
+import { Trip as TripStructure } from "../types.js";
 
 export const getTrips = async (
   req: Request,
@@ -32,18 +33,46 @@ export const deleteTripsById = async (
   const { idTrip } = req.params;
 
   try {
-    const tripToDelete = await Trip.findByIdAndDelete({ _id: idTrip}).exec();
+    const tripToDelete = await Trip.findByIdAndDelete({ _id: idTrip }).exec();
 
     if (!tripToDelete) {
       throw new CustomError(
         "Couldn't delete the trip.",
         500,
-        "Couldn't delete the trip selected."
-      )
+        "Couldn't delete the trip selected.",
+      );
     }
-    
-    res.status(200).json({idTrip});
+
+    res.status(200).json({ idTrip });
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const createTrip = async (
+  req: Request<
+    Record<string, unknown>, 
+    Record<string, unknown>, 
+    TripStructure
+  >,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const newTrip = req.body;
+
+    const tripToCreate = await Trip.create({ newTrip });
+
+    if (!tripToCreate) {
+      throw new CustomError(
+        "Couldn't create the trip.",
+        500,
+        "Couldn't create the trip."
+      )
+    }
+
+    res.status(201).json({ newTrip });
+  } catch (error) {
+    next(error);
+  }
+};
